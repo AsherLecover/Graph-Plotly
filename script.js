5700cbfbb93c6b21ffda1572302f184140bfa083
Asher Lecover <Asherlec5@gmail.con>
2020-12-23 01:38:37 +0200

    initail commit

--- script.js
+++ script.js
@@ -4,106 +4,99 @@ var movementFastData = [{x: [],y:[]}];
 var distanceData = [{x: [],y:[]}];
 
 var phaseData = new Array();
+var treatmentData = new Array();
 
 
 
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
+    // var request = new XMLHttpRequest();
+    // request.onreadystatechange = function() {
 
-      if (this.readyState == 4 && this.status == 200) {
-        var jsonObject = request.responseText.split(/\r?\n|\r/);
-        for (var i = 0; i < jsonObject.length; i++) {
-          csvData.push(jsonObject[i].split(','));
-        }
+    //   if (this.readyState == 4 && this.status == 200) {
+    //     var jsonObject = request.responseText.split(/\r?\n|\r/);
+    //     for (var i = 0; i < jsonObject.length; i++) {
+    //       csvData.push(jsonObject[i].split(','));
+    //     }
         
-        for (let i = 1 ; i <  csvData.length; i++) {
-            rpmData[0].x.push(csvData[i][0])
-            rpmData[0].y.push(csvData[i][2])
+    //     for (let i = 1 ; i <  csvData.length; i++) {
+    //         rpmData[0].x.push(csvData[i][0])
+    //         rpmData[0].y.push(csvData[i][2])
 
-            movementFastData[0].x.push(csvData[i][0])
-            movementFastData[0].y.push(parseInt( csvData[i][5]))
+    //         movementFastData[0].x.push(csvData[i][0])
+    //         movementFastData[0].y.push(parseInt( csvData[i][5]))
 
-            distanceData[0].x.push(csvData[i][0])
-            distanceData[0].y.push(csvData[i][3])
+    //         distanceData[0].x.push(csvData[i][0])
+    //         distanceData[0].y.push(csvData[i][3])
            
-           }
-        }
+    //        }
+    //     }
 
-    };
-    request.open("GET", 'https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/data_example.csv', false);   
-    request.send(null);  
+    // };
+    // request.open("GET", 'https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/data_example.csv', false);   
+    // request.send(null);  
 
 //-----------------------------------------------------------------------------------------------
 
 
-// var request = new XMLHttpRequest();
-// function get(url, callback) {
-//     request.onreadystatechange = function() {
-//       if (request.readyState === 4 && request.status === 200) {
-//         if ('function' === typeof callback) {
-//           callback(request.responseText);
-//         }
-//       }
-//     };
-//     request.open('GET', url, false)
-//     // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//     request.send();
-//   }
+var request = new XMLHttpRequest();
+function get(url, callback) {
+    request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        if ('function' === typeof callback) {
+          callback(request.responseText);
+        }
+      }
+    };
 
-//   get('https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/data_example.csv', function(jsonObject) {
+    request.open('GET', url, false)
+    request.send(null);
+  }
 
-//      jsonObject = request.responseText.split(/\r?\n|\r/);
-//     for (var i = 0; i < jsonObject.length; i++) {
-//       csvData.push(jsonObject[i].split(','));
-//     }
+  get('https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/data_example.csv', function(jsonObject) {
+
+     jsonObject = jsonObject.split(/\r?\n|\r/);
+    for (var i = 0; i < jsonObject.length; i++) {
+      csvData.push(jsonObject[i].split(','));
+    }
     
-//     for (let i = 1 ; i <  csvData.length; i++) {
-//         rpmData[0].x.push(csvData[i][0])
-//         rpmData[0].y.push(csvData[i][2])
+    for (let i = 1 ; i <  csvData.length; i++) {
+        rpmData[0].x.push(csvData[i][0])
+        rpmData[0].y.push(csvData[i][2])
 
-//         movementFastData[0].x.push(csvData[i][0])
-//         movementFastData[0].y.push(parseInt( csvData[i][5]))
+        movementFastData[0].x.push(csvData[i][0])
+        movementFastData[0].y.push(parseInt( csvData[i][5]))
 
-//         distanceData[0].x.push(csvData[i][0])
-//         distanceData[0].y.push(csvData[i][3])
+        distanceData[0].x.push(csvData[i][0])
+        distanceData[0].y.push(csvData[i][3])
        
-//        }
+       }
   
-//     get('https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/phase-example.csv', function(jsonObjectA) {
+    get('https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/phase-example.csv', function(jsonObjectPhase) {
 
-//          jsonObjectA = jsonObjectA.responseText.split(/\r?\n|\r/);
-//             for (var i = 0; i < jsonObjectA.length; i++) {
-//                 phaseData.push(jsonObjectA[i].split(','));
-//             }
-//     });
+      jsonObjectPhase = jsonObjectPhase.split(/\r?\n|\r/);
+            for (var i = 0; i < jsonObjectPhase.length; i++) {
+                phaseData.push(jsonObjectPhase[i].split(','));
+            }
+    });
+
+    get('https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/treatment-example.csv', function(jsonObjectTreatment) {
+
+      jsonObjectTreatment = jsonObjectTreatment.split(/\r?\n|\r/);
+            for (var i = 0; i < jsonObjectTreatment.length; i++) {
+              treatmentData.push(jsonObjectTreatment[i].split(','));
+            }
+    });
   
-//   });
+  });
 
 
-
-// var requesta = new XMLHttpRequest();
-// requesta.onreadystatechange = function() {
-
-//   if (this.readyState == 4 && this.status == 200) {
-//     var jsonObjectA = jsonObjectA.responseText.split(/\r?\n|\r/);
-//     for (var i = 0; i < jsonObjectA.length; i++) {
-//         phaseData.push(jsonObjectA[i].split(','));
-//     }
-    
-  
-//     }
-
-// };
-// requesta.open("GET", 'https://raw.githubusercontent.com/AsherLecover/Graph-Data/master/phase-example.csv', false);   
-// requesta.send(null); 
-
-// console.log('phaseData:', phaseData);
+console.log('treatmentData:', treatmentData);
 
 //----------------------------------------------------------------------------------------------
 
 var myDiv = document.getElementById("myDiv");
 var myDiv2 = document.getElementById("myDiv2");
 var myDiv3 = document.getElementById("myDiv3");
+var myDiv4 = document.getElementById("myDiv4");
 
 
 
@@ -140,9 +133,22 @@ Plotly.plot(myDiv, data, layoutRPM);
 Plotly.plot(myDiv2, data2, layoutMovementFast);
 Plotly.plot(myDiv3, data3, layoutDistance);
 
+var dataHeatmap = [
+  {
+    z: [[1, null, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, -10, 20]],
+    x: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    y: ['Morning', 'Afternoon', 'Evening'],
+    type: 'heatmap',
+    hoverongaps: false
+  }
+];
+
+Plotly.newPlot('myDiv4', dataHeatmap);
 
 
-var divs = [myDiv, myDiv2, myDiv3];
+
+
+var divs = [myDiv, myDiv2, myDiv3, myDiv4];
 
 function relayout(ed, divs) {
   divs.forEach((div, i) => {
